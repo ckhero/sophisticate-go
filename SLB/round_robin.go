c872749dcd3f9a7c63516335c655f86bbe110e52
ck <18801613198@163.com>
2020-08-11 08:41:49 +0800

    fix

--- SLB/round_robin.go
+++ SLB/round_robin.go
@@ -7,12 +7,14 @@ type RoundRobinBody struct {
 	currIndex int
 }
 
+type Next func() string
+
 func NewRoundRobin() *RoundRobinBody {
 	return &RoundRobinBody{
 		currIndex: 1,
 	}
 }
-func (r *RoundRobinBody) RoundRobin(nodes map[int]string) func() string {
+func (r *RoundRobinBody) RoundRobin(nodes map[int]string) Next {
 	var mtx sync.Mutex
 	return func() string {
 		mtx.Lock()
