e2b4e4c60cac827e89dff2371407236ab619f3ae
陈凯 <user@userdeMac-mini-3.local>
2020-08-26 17:38:00 +0800

    fux

---
+++ other/memory_align/memort_align_test.go
@@ -0,0 +1,13 @@
+/**
+* @Author: 人从众[ckhero]
+* @Date: 2020/8/26 4:05 下午
+* @Desc: a
+ */
+
+package memory_align
+
+import "testing"
+
+func TestOptimizeMemory(t *testing.T) {
+	OptimizeMemory()
+}
