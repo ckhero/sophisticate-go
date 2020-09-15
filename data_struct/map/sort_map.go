9f32e62ab13a9ea0cfe8534ea9409b5b06e307a7
ck <18801613198@163.com>
2020-09-15 23:35:05 +0800

    有序map

---
+++ data_struct/map/sort_map.go
@@ -0,0 +1,62 @@
+package _map
+
+import "container/list"
+
+type Keyer interface {
+	GetKey() string
+	GetVal() string
+}
+
+type MapList struct {
+	dataMap map[string]*list.Element
+	dataList  *list.List
+}
+
+func NewMapList() *MapList {
+	return &MapList{
+		dataMap:  make(map[string]*list.Element),
+		dataList: list.New(),
+	}
+}
+
+func (m *MapList) Exists(data Keyer) bool {
+	_, exists := m.dataMap[data.GetKey()]
+	return exists
+}
+
+func (m *MapList) Push(data Keyer) bool {
+	if m.Exists(data) {
+		return false
+	}
+	ele := m.dataList.PushBack(data)
+	m.dataMap[data.GetKey()] = ele
+	return true
+}
+
+func (m *MapList) Remove(data Keyer) {
+	if !m.Exists(data) {
+		return
+	}
+
+	m.dataList.Remove(m.dataMap[data.GetKey()])
+	delete(m.dataMap, data.GetKey())
+}
+
+func (m *MapList) Walk(cb func(data Keyer)) {
+	for ele := m.dataList.Front(); ele != nil; ele = ele.Next() {
+		cb(ele.Value.(Keyer))
+	}
+}
+
+type Element struct {
+	key string
+	value string
+}
+
+func (e *Element) GetKey() string {
+	return e.key
+}
+
+func (e *Element) GetVal() string {
+return e.value
+}
---
+++ data_struct/map/sort_map_test.go
@@ -0,0 +1,51 @@
+package _map
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+func TestSortMap(t *testing.T) {
+	m := map[string]string{
+		"1":"222",
+		"2":"2212",
+		"3":"2232",
+	}
+	keys := []string{}
+	for k, _ := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, v := range keys {
+		fmt.Println(m[v])
+	}
+}
+
+func TestSortedMap(t *testing.T) {
+	m := NewMapList()
+	var a,b,c Keyer
+	a = &Element{
+		key:   "aaa",
+		value: "aavvv",
+	}
+
+	b = &Element{
+		key:   "bbbb",
+		value: "bbvvv",
+	}
+
+	c = &Element{
+		key:   "ccc",
+		value: "cccvvv",
+	}
+	m.Push(a)
+	m.Push(b)
+	m.Push(c)
+
+	cb := func(data Keyer) {
+		fmt.Print(data.GetVal())
+		fmt.Println(data.GetKey())
+	}
+	m.Walk(cb)
+}
